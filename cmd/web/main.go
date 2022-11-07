fbb66e75bcc81f1320dbfd8f18f3d9ad815d970d
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-11-07 14:16:36 +0700

    add button test-broker & add gitignore

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,7 +12,7 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 80")
+	fmt.Println("Starting front end service on port 8085")
 	err := http.ListenAndServe(":8085", nil)
 	if err != nil {
 		log.Panic(err)
